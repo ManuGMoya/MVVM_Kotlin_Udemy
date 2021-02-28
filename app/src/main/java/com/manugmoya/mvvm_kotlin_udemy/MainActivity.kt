b217f543a44412a2c4ffbfd78b52cf62e07ff1da
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-02-28 19:01:02 +0100

    Initial commit

---
+++ app/src/main/java/com/manugmoya/mvvm_kotlin_udemy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.manugmoya.mvvm_kotlin_udemy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
