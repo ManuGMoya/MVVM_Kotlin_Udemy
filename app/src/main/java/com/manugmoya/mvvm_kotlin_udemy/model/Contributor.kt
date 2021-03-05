f583e56779beaa73ed503d26fb29a5eb97c91bc1
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-03-05 21:23:58 +0100

    Contributor entity created.

---
+++ app/src/main/java/com/manugmoya/mvvm_kotlin_udemy/model/Contributor.kt
@@ -0,0 +1,28 @@
+package com.manugmoya.mvvm_kotlin_udemy.model
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(
+    primaryKeys = ["repoName", "repoOwner", "login"],
+    foreignKeys = [ForeignKey(
+        entity = Repo::class, // Entidad con la que se va a relacionar.
+        parentColumns = ["name", "owner_login"], // Columnas de la Entidad con la que la relacionamos.
+        childColumns = ["repoName", "repoOwner"], // Columnas de esta propia clase/Entidad.
+        onUpdate = ForeignKey.CASCADE // Manera de actualización. Propaga la actualización en la clave padre e hijas.
+    )]
+)
+data class Contributor(
+
+    @field:SerializedName("login")
+    val login: String,
+    @field:SerializedName("contribution")
+    val contribution: Int,
+    @field:SerializedName("avatar_url")
+    val avatarUrl: String
+) {
+    lateinit var repoName: String
+    lateinit var repoOwner: String
+}
+
