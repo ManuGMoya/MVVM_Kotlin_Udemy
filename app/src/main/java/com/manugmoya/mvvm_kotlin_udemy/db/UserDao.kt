a043d9849d4361801601f80b5aa85bc8e9d818a4
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-03-06 11:55:57 +0100

    UserDao interface created.

---
+++ app/src/main/java/com/manugmoya/mvvm_kotlin_udemy/db/UserDao.kt
@@ -0,0 +1,19 @@
+package com.manugmoya.mvvm_kotlin_udemy.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.manugmoya.mvvm_kotlin_udemy.model.User
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: User)
+
+    @Query("SELECT * FROM user WHERE login = :login")
+    fun findByLogin(login: String): LiveData<User>
+
+}
