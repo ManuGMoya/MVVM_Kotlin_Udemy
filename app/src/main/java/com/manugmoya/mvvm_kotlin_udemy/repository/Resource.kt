f0e4324357b003f150dcaa98f2f1f518abc5c05e
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-03-07 13:34:56 +0100

    Status enum class created. Resource data class created to keep status.

---
+++ app/src/main/java/com/manugmoya/mvvm_kotlin_udemy/repository/Resource.kt
@@ -0,0 +1,17 @@
+package com.manugmoya.mvvm_kotlin_udemy.repository
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+    companion object {
+        fun <T> sucess(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+}
---
+++ app/src/main/java/com/manugmoya/mvvm_kotlin_udemy/repository/Status.kt
@@ -0,0 +1,7 @@
+package com.manugmoya.mvvm_kotlin_udemy.repository
+
+enum class Status {
+    LOADING,
+    SUCCESS,
+    ERROR
+}
