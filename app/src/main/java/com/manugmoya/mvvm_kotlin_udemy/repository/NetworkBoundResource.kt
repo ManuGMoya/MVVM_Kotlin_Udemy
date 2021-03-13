a8d77d249f14e7bc28f31f679328bf9c4e4b6977
Manuel Gutierrez Moya <manuel.gutierrez.moya@everis.com>
2021-03-13 18:50:08 +0100

    NetworkBoundResource abstract class created.

---
+++ app/src/main/java/com/manugmoya/mvvm_kotlin_udemy/repository/NetworkBoundResource.kt
@@ -0,0 +1,102 @@
+package com.manugmoya.mvvm_kotlin_udemy.repository
+
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.manugmoya.mvvm_kotlin_udemy.AppExecutors
+import com.manugmoya.mvvm_kotlin_udemy.api.ApiEmptyResponse
+import com.manugmoya.mvvm_kotlin_udemy.api.ApiErrorResponse
+import com.manugmoya.mvvm_kotlin_udemy.api.ApiResponse
+import com.manugmoya.mvvm_kotlin_udemy.api.ApiSuccessResponse
+
+abstract class NetworkBoundResource<ResultType, RequestType>
+@MainThread constructor(private val appExecutors: AppExecutors) {
+    private val result = MediatorLiveData<Resource<ResultType>>()
+
+    init {
+        result.value = Resource.loading(null)
+        val dbSource = loadFromDb()
+        result.addSource(dbSource) { data ->
+            result.removeSource(dbSource)
+            if (shouldFetch(data)) {
+                fetchFromNetwork(dbSource)
+            } else {
+                result.addSource(dbSource) { newData ->
+                    setValue(Resource.sucess(newData))
+                }
+            }
+        }
+    }
+
+    @MainThread
+    private fun setValue(newValue: Resource<ResultType>) {
+        if (result.value != newValue) {
+            result.value = newValue
+        }
+    }
+
+    private fun fetchFromNetwork(dbSource: LiveData<ResultType>) {
+        val apiResponse = createCall()
+        result.addSource(dbSource) { newData ->
+            setValue(Resource.loading(newData))
+        }
+        result.addSource(apiResponse) { response ->
+            result.removeSource(apiResponse)
+            result.removeSource(dbSource)
+            when (response) {
+                is ApiSuccessResponse -> {
+                    appExecutors.diskIO.execute {
+                        saveCallResult(processResponse(response))
+                        appExecutors.mainThread().execute {
+                            result.addSource(loadFromDb()) { newData ->
+                                setValue(Resource.sucess(newData))
+
+                            }
+                        }
+                    }
+                }
+                is ApiEmptyResponse -> {
+                    appExecutors.mainThread().execute {
+                        result.addSource(loadFromDb()) { newData ->
+                            setValue(Resource.sucess(newData))
+                        }
+                    }
+                }
+                is ApiEmptyResponse -> {
+                    onFetchFailed()
+                    result.addSource(dbSource) { newData ->
+                        setValue(
+                            Resource.error(
+                                (response as ApiErrorResponse).errorMessage,
+                                newData
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    protected open fun onFetchFailed() {
+
+    }
+
+    fun asLiveData() = result as LiveData<Resource<ResultType>>
+
+    @WorkerThread
+    protected open fun processResponse(response: ApiSuccessResponse<RequestType>) = response.body
+
+    @WorkerThread
+    protected abstract fun saveCallResult(item: RequestType)
+
+
+    @MainThread
+    protected abstract fun shouldFetch(data: ResultType?): Boolean
+
+    @MainThread
+    protected abstract fun loadFromDb(): LiveData<ResultType>
+
+    @MainThread
+    protected abstract fun createCall(): LiveData<ApiResponse<RequestType>>
+}
